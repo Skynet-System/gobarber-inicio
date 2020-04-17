bcd5e305a3142863b472c914e1de96998eba7c06
Alex Sodre <skynetsistema2020@gmail.com>
2020-04-17 17:59:46 -0300

    new commit

---
+++ modulo01/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const server = express();
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  return res.json({ message:`Buscando o Usuário ${id}` });
+})
+
+server.listen(3000);
